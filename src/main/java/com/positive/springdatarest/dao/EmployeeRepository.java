d1d42f95f3c8a4e459d05e7e63a6a7e6b9518f31
Subhranshu Pujari <subh.pujari@gmail.com>
2020-06-21 13:39:20 +0530

    rest repostories path added

--- src/main/java/com/positive/springdatarest/dao/EmployeeRepository.java
+++ src/main/java/com/positive/springdatarest/dao/EmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.positive.springdatarest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.positive.springdatarest.model.Employee;
-
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
